aeab738268af244ca468ffbd416d9dc1ba5fa7e8
luismedellin1989 <luiseduardo1218@gmail.com>
2016-02-16 14:48:11 -0500

    Primera prueba React

---
+++ src/components/homePage.js
@@ -0,0 +1,18 @@
+"use strict"; //evaluar todo lo que estamos haciendo en modo estricto
+
+
+var Home = React.createClass({
+	render: function (){
+		return (
+			<div className='container'>
+				<h1>Pluralsight Administration</h1>
+				<p>React, React Router, and Flux for ultra-responsive web apps.</p>
+				<button className='btn btn-primary' />
+			</div>
+		);
+	}
+});
+
+console.log('Prueba');
+
+module.exports = Home;
